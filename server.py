12c85850e767bb205b4c30e1107dfabea314e196
Colin Thomas <contact@nwa2coco.fr>
2019-05-05 10:14:14 +0200

    Adding code on github

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import glob
+import sys
 import os
 import threading
 import time
+import codecs
+import json
 from datetime import datetime
 
-from flask import Flask, request
+from flask import Flask, request, send_file, make_response
 from pymessenger.bot import Bot
 
 
@@ -15,14 +18,15 @@ class Verif(threading.Thread):
     def run(self):
         while True:
             print("Launch Automatic Verification")
+            print("Rev")
             if os.path.exists("/home/servs/saves/lastsentmessage.save"):
                 verification()
-            time.sleep(60 * 60 * 6)
+            time.sleep(6 * 3600)
 
 
 app = Flask(__name__)
-ACCES_TOKEN = "EAAFqeesT4ZB0BAKBegdekleyYEuw4bwVBdHtVyOMSaEnnzZCNUFDassNeXp7wflwdMeNwG3GPQx2k3WfvcuJTgTaq2rJQVIJhnL05avRK2J9OM7ERQ7VNCdRQgEZC8PrUi7JMOk3OGogRwYRTYAcZCs92wlvzeSvjgNsKKuesgZDZD"
-ID = "2564006353628871"
+ACCES_TOKEN = "TOKEN"
+ID = "CO_ID"
 VERIFY_TOKEN = "This_Is_A_Token_@"
 bot = Bot(ACCES_TOKEN)
 
@@ -36,6 +40,7 @@ def getAll():
 
 def test():
     bot.send_text_message(ID, "Test#01")
+
 
 @app.route("/", methods=['GET', 'POST'])
 def receive():
@@ -64,6 +69,18 @@ def d():
         return save_uvrim(ob['scc'], ob['name'], ob['uvrim'])
 
 
+@app.route("/latest.jar")
+def lsercs():
+    return send_file("/home/servs/versions/latest/UAS_V." + erLa() + ".jar")
+
+
+@app.route("/start_auto")
+def start_auto():
+    t = Verif()
+    t.start()
+    return "true"
+
+
 @app.route("/user_register", methods=['POST'])
 def ref():
     if request.method == 'POST':
@@ -77,9 +94,16 @@ def e():
     if request.method == 'GET':
         return str(isSent())
 
+
 @app.route("/hello")
 def sle():
-	return "Hello World"
+    return "Hello World"
+
+
+@app.route("/get_latest")
+def sler():
+    return erLa()
+
 
 def verif(token):
     if token == VERIFY_TOKEN:
@@ -91,7 +115,7 @@ def register_user(scc, name):
     if os.path.exists("/home/servs/saves/" + scc + "." + name + ".uvrim"):
         print("exist")
     else:
-        fichier = open("/home/servs/saves/" + scc + "." + name + ".uvrim", "x")
+        fichier = open("/home/servs/saves/" + scc + "." + name + ".uvrim", "x", encoding="utf-8")
         fichier.close()
 
 
@@ -100,14 +124,25 @@ def save_uvrim(scc, name, uvrim):
         return "Error"
     else:
         os.remove("/home/servs/saves/" + scc + "." + name + ".uvrim")
-        fichier = open("/home/servs/saves/" + scc + "." + name + ".uvrim", "x")
+        fichier = open("/home/servs/saves/" + scc + "." + name + ".uvrim", "x", encoding="utf-8")
         fichier.close()
-        fichier = open("/home/servs/saves/" + scc + "." + name + ".uvrim", "w")
+        fichier = open("/home/servs/saves/" + scc + "." + name + ".uvrim", "w", encoding="utf-8")
         fichier.write(uvrim)
         fichier.flush()
         fichier.close()
         verification()
     return "save success"
+
+
+# LATEST V /home/servs/versions/UAS_V.<VERSION>.<NAME>.jar
+def erLa():
+    print("get_latest")
+    la = ""
+    for files in glob.iglob("/home/servs/versions/latest/*.jar"):
+        la = os.path.basename(open(files).name)
+    la = la.replace("UAS_V.", "")
+    la = la.replace(".jar", "")
+    return la;
 
 
 def verification():
@@ -126,9 +161,23 @@ def verification():
             bot.send_text_message(ID, st)
             time.sleep(10)
         remake()
+        remake_files()
         return True
     else:
         return False
+
+
+@app.route('/manual_launch')
+def manual_launch():
+    print("envoi")
+    list = []
+    for files in glob.iglob("/home/servs/saves/*.uvrim"):
+        list.append(open(files).read())
+    for st in list:
+        bot.send_text_message(ID, st)
+        time.sleep(10)
+    remake()
+    return True
 
 
 def isSent():
@@ -148,6 +197,29 @@ def isSent():
             return False
 
 
+@app.route('/syncronize', methods=['POST'])
+def syncronize():
+    arr = request.get_json();
+    scc = arr['scc'];
+    name = arr['name']
+
+    if not os.path.exists("/home/servs/saves/" + scc + "." + name + ".uvrim"):
+        return "Error";
+
+    array = [];
+    with open("/home/servs/saves/" + scc + "." + name + ".uvrim") as file:
+        isf = 0;
+        for line in file:
+            if isf == 0 or isf == 1:
+                continue;
+            array.append(line);
+            isf += 1;
+    print(array.count())
+    s = "\\n";
+
+    return str(array);
+
+
 def remake():
     if not os.path.exists("/home/servs/saves/lastsentmessage.save"):
         return False
@@ -163,8 +235,34 @@ def remake():
         return True
 
 
+def remake_file(file):
+    path = os.path.abspath(file)
+    name = os.path.basename(path)
+    name = name.replace(".uvrim", "")
+    s = name.split(".")
+    os.remove(path)
+    fichier = open(path, "x")
+    fichier.close()
+    fichier = open(path, "w")
+    nam = str(str(s[0]) + str("\n") + str("Nom : " + str(s[1]) + str("\n") + str("Tout vas bien")))
+    fichier.write(nam)
+    fichier.flush()
+    fichier.close()
+    return True
+
+
 def test():
     bot.send_text_message(ID, "Test")
+
+
+@app.route("/remake_files")
+def rmkfi():
+    remake_files()
+
+
+def remake_files():
+    for files in glob.iglob("/home/servs/saves/*.uvrim"):
+        remake_file(files)
 
 
 if __name__ == '__main__':
